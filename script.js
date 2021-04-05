a2dc0cb8e5f1ba9cba07c1b475bcf635f1c4f8c9
Andrew Broughton <a.broughton@me.com>
2021-04-05 16:03:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+      <div class="key">
+      ${e.key === ' ' ? 'Space' : e.key}
+      <small>event.key</small>
+      </div>
+      <div class="key">
+        ${e.keyCode}
+          <small>evnet.keycode</small>
+      </div>
+      <div class="key">
+          ${e.code}
+          <small>event.code</small>
+      </div>
+      <div class="key">
+          <h2>Press any key code to get the code</h2>
+      </div>
+  `
+})
